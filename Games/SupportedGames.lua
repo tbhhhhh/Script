a8e9b7223c44f78a6c5c0b2dc9ebb48d6e651f17
Xingtaiduan <131981549+Xingtaiduan@users.noreply.github.com>
2025-07-17 01:28:40 +0800

    Update SupportedGames.lua

--- Games/SupportedGames.lua
+++ Games/SupportedGames.lua
@@ -1,36 +1,5 @@
 local SupportedGames = {
-    --A
-    --B
-    ["BladeBall"] = "刀刃球",
-    ["BuildABoatForTreasure"] = "造船寻宝",
-    --C
-    --D
-    ["Doors"] = "Doors",
-    --E
-    --F
-    ["FPE%3AS"] = "FPE:S",
-    --G
-    ["Granny"] = "奶奶",
-    --H
-    ["HideAndSeekExtreme"] = "极限捉迷藏",
-    --I
-    --J
-    --K
-    --L
-    --M
-    ["MurderMystery2"] = "破坏者谜团2",
-    --N
-    ["NaturalDisasterSurvival"] = "自然灾害",
-    ["NavalWarfare"] = "海战",
-    --O
-    ["Ohio"] = "俄亥俄州",
-    --P
-    ["Pressure"] = "压力",
-    ["PrisonLife"] = "监狱人生",
-    --Q
-    --R
-    ["RainbowFriends1"] = "彩虹朋友1",
-    ["RainbowFriends2"] = "彩虹朋友2"
+    ["N/A"] = "脚本重做中..."
 }
 
 return SupportedGames
