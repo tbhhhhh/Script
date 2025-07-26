62c90e8c673f6e6db869cd76a9a1693a1b48cb99
Xingtaiduan <131981549+Xingtaiduan@users.noreply.github.com>
2025-07-27 01:39:23 +0800

    Update ESPLibrary.lua

--- ESPLibrary.lua
+++ ESPLibrary.lua
@@ -33,8 +33,16 @@ local Library = {
     Tags = {},
     Connections = {},
     ESPFolder = Instance.new("Folder", CoreGui),
-    ShowDistance = true,
-    MaxDistance = math.huge
+    DefaultSettings = {
+        Name = "Unnamed",
+        Color = Color3.new(1, 1, 1),
+        TextSize = 15,
+        Tag = "DefaultTag",
+        ShowTextLabel = true,
+        ShowHighlight = true,
+        ShowDistance = true,
+        MaxDistance = math.huge,
+    }
 }
 
 Library.ESPFolder.Name = "ESPFolder"
@@ -46,63 +54,92 @@ Library.Add = function(...)
         local object, name, color, size, tag = ...
         espSettings = {Object = object,Name = name,Color = color,TextSize = size,Tag = tag}
     end
-    if espSettings.ShowDistance == nil then espSettings.ShowDistance = Library.ShowDistance end
-    if not espSettings.MaxDistance then espSettings.MaxDistance = Library.MaxDistance end
-    if not espSettings.Color then espSettings.Color = Color3.new(1, 1, 1) end
-    if not espSettings.TextSize then espSettings.TextSize = 15 end
+    
+    assert(espSettings.Object, "missing esp object")
+    for i, v in pairs(Library.DefaultSettings) do
+        if espSettings[i] == nil then
+            espSettings[i] == v
+        end
+    end
     
     local ESP = {
         Index = #Library.ESP+1,
         Settings = espSettings,
         Instances = {}
-    }   
+    }
+    ESP.Instances.Folder = Instance.new("Folder", Library.ESPFolder)
+    ESP.Instances.Folder.Name = ESP.Settings.Tag
+    
     if Library.Tags[ESP.Settings.Tag] == nil then
         Library.Tags[ESP.Settings.Tag] = true
     end
     
-    local BillboardGui = Instance.new("BillboardGui", Library.ESPFolder)
-    BillboardGui.Name = ESP.Settings.Tag
-    BillboardGui.Enabled = false
-    BillboardGui.ResetOnSpawn = false
-    BillboardGui.AlwaysOnTop = true
-    BillboardGui.Size = UDim2.new(0, 200, 0, 50)
-    BillboardGui.Adornee = ESP.Settings.Object
-    BillboardGui.StudsOffset = Vector3.new(0, 0, 0)
+    local BillboardGui
+    local TextLabel
+    if ESP.Settings.ShowTextLabel then
+        BillboardGui = Instance.new("BillboardGui", ESP.Instances.Folder)
+        BillboardGui.Name = ESP.Settings.Tag
+        BillboardGui.Enabled = false
+        BillboardGui.ResetOnSpawn = false
+        BillboardGui.AlwaysOnTop = true
+        BillboardGui.Size = UDim2.new(0, 200, 0, 50)
+        BillboardGui.Adornee = ESP.Settings.Object
+        BillboardGui.StudsOffset = Vector3.new(0, 0, 0)
+        
+        TextLabel = Instance.new("TextLabel", BillboardGui)
+        TextLabel.Size = UDim2.new(1, 0, 1, 0)
+        TextLabel.Font = Enum.Font.SourceSans
+        TextLabel.TextWrapped = true
+        TextLabel.RichText = true
+        TextLabel.TextStrokeTransparency = 0.5
+        TextLabel.BackgroundTransparency = 1
+        TextLabel.Text = ESP.Settings.Name
+        TextLabel.TextColor3 = ESP.Settings.Color
+        TextLabel.TextSize = ESP.Settings.TextSize
+        Instance.new("UIStroke", TextLabel)
+        
+        ESP.Instances.BillboardGui = BillboardGui
+        ESP.Instances.TextLabel = TextLabel
+    end
     
-    local TextLabel = Instance.new("TextLabel", BillboardGui)
-    TextLabel.Size = UDim2.new(1, 0, 1, 0)
-    TextLabel.Font = Enum.Font.SourceSans
-    TextLabel.TextWrapped = true
-    TextLabel.RichText = true
-    TextLabel.TextStrokeTransparency = 0.5
-    TextLabel.BackgroundTransparency = 1
-    TextLabel.Text = ESP.Settings.Name
-    TextLabel.TextColor3 = ESP.Settings.Color
-    TextLabel.TextSize = ESP.Settings.TextSize
+    local Highlight
+    if ESP.Settings.ShowHighlight then
+        Highlight = Instance.new("Highlight", ESP.Instances.Folder)
+        Highlight.Adornee = nil
+        Highlight.FillColor = ESP.Settings.Color
+        Highlight.OutlineColor = ESP.Settings.Color
+        Highlight.FillTransparency = 0.65
+        Highlight.OutlineTransparency = 0
+        
+        ESP.Instances.Highlight = Highlight
+    end
     
-    local UIStroke = Instance.new("UIStroke", TextLabel)
-    UIStroke.Thickness = 1
-    
-    local Highlight = Instance.new("Highlight", BillboardGui)
-    Highlight.Adornee = nil
-    Highlight.FillColor = ESP.Settings.Color
-    Highlight.OutlineColor = ESP.Settings.Color
-    Highlight.FillTransparency = 0.65
-    Highlight.OutlineTransparency = 0
-    
-    ESP.Instances.BillboardGui = BillboardGui
-    ESP.Instances.TextLabel = TextLabel
-    ESP.Instances.UIStroke = UIStroke
-    ESP.Instances.Highlight = Highlight
+    function ESP:Update(newSettings)
+        for i, v in pairs(newSettings) do
+            ESP.Settings[i] = v
+            if TextLabel then
+                TextLabel.TextColor3 = ESP.Settings.Color
+                TextLabel.TextSize = ESP.Settings.TextSize
+            end
+            if Highlight then
+                Highlight.FillColor = ESP.Settings.Color
+                Highlight.OutlineColor = ESP.Settings.Color
+            end
+        end
+    end
     
     function ESP:Destroy()
-        if BillboardGui then BillboardGui:Destroy() end
+        ESP.Instances.Folder:Destroy()
         Library.ESP[ESP.Index] = nil
     end
     
     function ESP:ToggleVisibility(Value)
-        BillboardGui.Enabled = Value
-        Highlight.Adornee = Value and ESP.Settings.Object or nil
+        if BillboardGui then
+            BillboardGui.Enabled = Value
+        end
+        if Highlight then
+            Highlight.Adornee = Value and ESP.Settings.Object or nil
+        end
     end
     ESP:ToggleVisibility(Library.Tags[ESP.Settings.Tag])
 
@@ -114,12 +151,24 @@ Library.SetEnabled = function(tag, value)
     Library.Tags[tag] = value
 end
 
-Library.Clear = function(tag)
+Library.ForEachTag = function(tag, callback)
     for _, ESP in pairs(Library.ESP) do
         if ESP.Settings.Tag == tag then
-            ESP:Destroy()
+            callback(ESP)
         end
     end
+end
+
+Library.UpdateESP = function(tag, newSettings)
+    Library.ForEachTag(tag, function(ESP)
+        ESP:Update(newSettings)
+    end)
+end
+
+Library.Clear = function(tag)
+    Library.ForEachTag(tag, function(ESP)
+        ESP:Destroy()
+    end)
 end
 
 Library.Destroy = function()
@@ -161,10 +210,12 @@ table.insert(Library.Connections, RunService.RenderStepped:Connect(function()
             continue
         end
         
-        if ESP.Settings.ShowDistance then
-            ESP.Instances.TextLabel.Text = ("%s\n[%s]"):format(ESP.Settings.Name, math.floor(Distance))
-        else
-            ESP.Instances.TextLabel.Text = ESP.Settings.Name
+        if ESP.Settings.ShowTextLabel then
+            if ESP.Settings.ShowDistance then
+                ESP.Instances.TextLabel.Text = ("%s\n[%s]"):format(ESP.Settings.Name, math.floor(Distance))
+            else
+                ESP.Instances.TextLabel.Text = ESP.Settings.Name
+            end
         end
     end
 end))
